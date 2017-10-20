18cd8f68789b30ba97e8de48fc7fbc7306cbb231
jcmijas <escandoncastillojuancarlos@gmail.com>
2017-10-20 12:40:17 -0700

    deploy.sh

--- codedeploy/deploy.sh
+++ codedeploy/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 aws deploy --profile sandbox create-deployment \
---application-name api_gseanalytics \
+--application-name CI_api_gseanalytics \
 --deployment-config-name CodeDeployDefault.OneAtATime \
---deployment-group-name api_gseanalytics_gn \
+--deployment-group-name CI_api_gseanalytics_Deployment_Group \
 --description "Main Api GSE Analytics Deployment" \
 --ignore-application-stop-failures \
 --github-location repository=jcmijas/codedeploy,commitId=$(git rev-parse HEAD)
